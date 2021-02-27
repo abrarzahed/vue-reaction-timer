64ad8eb68ba3f41663294cce5e4ba374bb14b261
Abrar Hussen Zahed <abrarzahed6986@gmail.com>
2021-02-27 21:01:50 +0600

    commited all

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<p>Lets see how quick you can react👇🏻</p>
+  <h1>Awesome Reaction Timer</h1>
+  <button class="init" @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Result v-if="showResult" :score="score"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Block, Result },
+
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score : null,
+      showResult : false,
+    };
+  },
+
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResult = false
+    },
+    endGame(rt){
+      this.score = rt
+      this.isPlaying = false
+      this.showResult= true
+    }
+  },
+};
 </script>
 
 <style>
@@ -20,7 +44,17 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+.init{
+  background: rgb(4, 255, 129);
+  border: 0;
+  padding: 15px 30px;
+  font-size: 18px;
+  border-radius: 10px;
+}
+.init:disabled{
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,33 @@
+<template>
+  <p>Reaction Time - {{ score }}</p>
+  <p class="rank"> {{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data (){
+       return{
+           rank : null
+       }
+    },
+    mounted(){
+        if(this.score < 250){
+            this.rank = 'Monstar😎'
+        }else if(this.score < 400){
+            this.rank = 'Rapid Action😲'
+        }else{
+            this.rank = 'Slow😢'
+        }
+    }
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #016d6d;
+}
+
+</style>
